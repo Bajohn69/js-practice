34e884a52b9119941e4de321eb9bfc0752e0bd1a
Bajohn Huang <jimmybaga@hotmail.com>
2022-11-23 16:41:50 +0800

    node

---
+++ 342.CMD, git.js
@@ -0,0 +1,48 @@
+/**
+ * CMD 名詞解釋
+ * cd = change directory(directory = folder)
+ * dir = directory (看當前所有資料夾)
+ * mkdir = make directory
+ * cd .. = 上一個資料夾
+ * cd d + tab = Desktop
+ * type nul 製作文件類型(nul 沒有類型)
+ * type nul > 'index.html'
+ * type nul > 'app.js'
+ * cls 讓 cmd 變乾淨
+ * code 開 vs code
+ *
+ * GIT Bash
+ * pwd = print working directory(directory = folder)
+ * ls = list all files inside directory
+ * touch = 建立檔案 (touch index.html)
+ * rm = remove file(rm index.html)
+ * rmdir = 移除資料夾
+ * rm -rf = 強制刪除資料夾與裡面的檔案 (f = force)
+ * ctrl + l = 清除頁面(但往上滑他還在)
+ * code (project名稱) 用 vs code 開 project
+ *
+ * Git command
+ * git init = initialized empty git repository (git 在隱藏項目)
+ * git --version = git 版本
+ * git config --global user.name "Bajohn"
+ * git config --global user.email "email"
+ * git config --list 看有沒有 user.name, user.email
+ * git status = git 狀態
+ * git add index.html
+ * git add.
+ * git commit -m 'add codes into index.html and app.js'
+ * git rm --cached 'index.html' = 從 staged 移除
+ * git add *.html = 放入所有 .html 文件
+ * git log = git 歷史
+ * gitignore
+ * git branch login(分支名)
+ * git chechout login = switched to branch 'login'
+ * git chechout master/main = switched to branch 'master'
+ * git merge login
+ * merge
+ * 1.一定要先 add, commit
+ * 2.conflict
+ */
+
+console.log(__filename);
+console.log(__dirname);
---
+++ Node.js/try1.js
@@ -0,0 +1,5 @@
+function morning(name) {
+  console.log("Good morning, " + name);
+}
+
+console.log(module);
