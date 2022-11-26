2f80b8b415c281f644a7192f1059d1dbb696cde8
Bajohn Huang <jimmybaga@hotmail.com>
2022-11-26 17:12:24 +0800

    node, cowsay, nodemon

--- Node.js/365.NodeServer.js
+++ Node.js/365.NodeServer.js
@@ -1,13 +1,28 @@
 const http = require("http");
+const url = require("url");
+const fs = require("fs");
+const path = require("path");
 
 const server = http.createServer((req, res) => {
   // req 使用者 request
   // res response
   //   console.log(req); 會出現超多東東
-  console.log(req.url);
-
-  res.write("Hello user.");
-  res.end();
+  // console.log(req.url); // Server is running on port 3501.
+  if (req.url == "/") {
+    fs.readFile(path.join(__dirname, "index.html"), (err, data) => {
+      res.writeHead(200, { "Content-Type": "text / html" });
+      res.write(data);
+      res.end();
+    });
+    // res.writeHead(200, { "Content-Type": "text / html" });
+    // res.write("<h1>You are on the homepage.</h1>");
+    // res.write("<p>Hello</p>");
+    // res.end();
+  } else {
+    let parseURL = url.parse(req.url); // 出現網頁就可以在網址後面亂打
+    res.write("Hello user" + parseURL.pathname);
+    res.end();
+  }
 });
 
 server.listen(3501, () => {
