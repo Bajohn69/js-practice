43735c13bf66abb9f84f044dfd33fac52c52bffb
Bajohn Huang <jimmybaga@hotmail.com>
2022-12-05 17:32:03 +0800

    update data

--- student project/app.js
+++ student project/app.js
@@ -4,9 +4,11 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const Student = require("./models/students");
+const methodOverride = require("method-override");
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 app.set("view engine", "ejs");
 
 mongoose
@@ -64,7 +66,7 @@ app.get("/students/:id", async (req, res) => {
   let { id } = req.params;
   try {
     let data = await Student.findOne({ id });
-    console.log(data);
+    // console.log(data);
     // 由於 findOne() 會回傳 null 讓 catch 無效(沒有出現相對應的頁面)，所以要做下面的事
     if (data !== null) {
       res.render("studentPage.ejs", { data });
@@ -72,8 +74,39 @@ app.get("/students/:id", async (req, res) => {
       res.send("Cannot find this student. Please enter a valid id.");
       // 雖然會出現這一頁但 catch 還是有它存在的必要
     }
+  } catch {
+    res.send("Error!");
+  }
+});
+
+app.get("/students/edit/:id", async (req, res) => {
+  let { id } = req.params;
+  try {
+    let data = await Student.findOne({ id });
+    if (data !== null) {
+      res.render("edit.ejs", { data });
+    } else {
+      res.send("Cannot find student.");
+    }
+  } catch {
+    res.send("Error!");
+  }
+});
+
+app.put("/students/edit/:id", async (req, res) => {
+  // console.log(req.body);
+  // res.send("Thanks for sending put request.");
+  // let { id } = req.params;
+  let { id, name, age, merit, other } = req.body;
+  try {
+    let d = await Student.findOneAndUpdate(
+      { id },
+      { id, name, age, scholarship: { merit, other } },
+      { new: true, runValidators: true }
+    );
+    res.redirect(`/students/${id}`);
   } catch (e) {
-    res.send("Error!", e);
+    res.render("reject.ejs");
   }
 });
 
