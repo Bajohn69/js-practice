1a5d18e5ac7bfe1cf007c2619ffc869076e3d19e
Bajohn Huang <jimmybaga@hotmail.com>
2023-01-09 11:36:07 +0800

    recipe, firebase

--- Recipe/recipe/src/App.js
+++ Recipe/recipe/src/App.js
@@ -6,16 +6,18 @@ import RecipeTile from "./RecipeTile";
 function App() {
   const [query, setQuery] = useState("");
   const [recipes, setRecipes] = useState([]);
+  const [healthLabels, setHealthLabels] = useState("vegan");
 
   const ID = process.env.REACT_APP_ID;
   const KEY = process.env.REACT_APP_KEY;
 
-  const URL = `https://api.edamam.com/search?q=${query}&app_id=${process.env.REACT_APP_ID}&app_key=${process.env.REACT_APP_KEY}&&health=alcohol-free`;
+  const URL = `https://api.edamam.com/search?q=${query}&app_id=${ID}&app_key=${KEY}&health=${healthLabels}`;
 
   const getRecipes = async function () {
     const result = await Axios.get(URL);
     setRecipes(result.data.hits); // return an array
     console.log(result.data);
+    console.log(URL);
   };
 
   const onSubmit = (e) => {
@@ -33,6 +35,32 @@ function App() {
           value={query}
           onChange={(e) => setQuery(e.target.value)}
         />
+        <select className="healthLabels">
+          <option onClick={() => setHealthLabels("vegan")}>vegan</option>
+          <option onClick={() => setHealthLabels("vegetarian")}>
+            vegetarian
+          </option>
+          <option onClick={() => setHealthLabels("soy-free")}>soy-free</option>
+          <option onClick={() => setHealthLabels("peanut-free")}>
+            peanut-free
+          </option>
+          <option onClick={() => setHealthLabels("wheat-free")}>
+            wheat-free
+          </option>
+          <option onClick={() => setHealthLabels("dairy-free")}>
+            dairy-free
+          </option>
+          <option onClick={() => setHealthLabels("egg-free")}>egg-free</option>
+          <option onClick={() => setHealthLabels("gluten-free")}>
+            gluten-free
+          </option>
+          <option onClick={() => setHealthLabels("low-sugar")}>
+            low-sugar
+          </option>
+          <option onClick={() => setHealthLabels("alcohol-free")}>
+            alcohol-free
+          </option>
+        </select>
         <input type="submit" value="Search" className="submit" />
       </form>
 
--- Recipe/recipe/src/RecipeTile.js
+++ Recipe/recipe/src/RecipeTile.js
@@ -3,7 +3,12 @@ import React from "react";
 export default function RecipeTile({ recipe }) {
   return (
     // recipe["recipe"]["image"].match(/\.(jpeg|jpg|gif|png)$/) != null && // 就不會出現 SVG 檔
-    <div className="recipeTile col">
+    <div
+      className="recipeTile col"
+      onClick={() => {
+        window.open(recipe["recipe"]["url"]);
+      }}
+    >
       <div className="img-wrap">
         <img className="img" src={recipe["recipe"]["image"]} />
       </div>
