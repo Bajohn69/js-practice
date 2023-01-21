89d88f6b4d8e8eec6188f7ead6688d890c3c59db
Bajohn Huang <jimmybaga@hotmail.com>
2023-01-22 01:53:25 +0800

    next css

--- next-practice/components/layout.js
+++ next-practice/components/layout.js
@@ -1,23 +1,30 @@
 import Head from "next/head";
 import Link from "next/link";
+import styles from "./layout.module.css";
 
 const name = "Bajohn";
 const webSiteTitle = "Next.js practice";
 
 export default function Layout({ children, returnBack }) {
   return (
-    <div>
+    <div className={styles.layout}>
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <meta name="author" content="Bajohn" />
       </Head>
       {/* next 有一個自己的 Head 標籤，要 import */}
-      <h1>{webSiteTitle}</h1>
-      <h2>Author: {name}</h2>
+      <header className={styles.header}>
+        <h1>{webSiteTitle}</h1>
+        <h2>Author: {name}</h2>
+      </header>
 
       <main>{children}</main>
-      {returnBack && <Link href="/">Back to Home</Link>}
+      {returnBack && (
+        <Link className={styles.home} href="/">
+          Back to Home
+        </Link>
+      )}
     </div>
   );
 }
---
+++ next-practice/src/pages/_app.js
@@ -0,0 +1,7 @@
+import "../../styles/global.css";
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+  //   add global CSS
+  //   https://nextjs.org/docs/advanced-features/custom-app
+}
