26ac7e0025a12345a46caeee504207845aa78997
Bajohn Huang <jimmybaga@hotmail.com>
2023-10-28 16:57:30 +0800

    1028

---
+++ js-0201/index.js
@@ -0,0 +1,11 @@
+// document.getElementById('total').addEventListener('click', function () {
+//     document.getElementById('total').innerText = Number(document.getElementById('total').innerText)+1
+// })
+
+document.getElementById('plus').addEventListener('click', function () {
+    document.getElementById('total').innerText = Number(document.getElementById('total').innerText)+1
+})
+
+document.getElementById('minus').addEventListener('click', function () {
+    document.getElementById('total').innerText = Number(document.getElementById('total').innerText)-1
+})
